e9a5ce003f224ecd6d537f7346aac4d69e8d11ec
Author Michael Han <32470787+oxym@users.noreply.github.com>
2022-12-07 02:54:23 -0500

    day1 & day6

---
+++ 1/core.clj
@@ -0,0 +1,13 @@
+(require '[clojure.string :as str])
+
+(def input (slurp "input.txt"))
+(def elves (str/split input  #"\n\n"))
+
+(defn get-calories [elf] (map read-string (str/split elf #"\n")))
+(defn sum-calories [calories] (reduce + calories))
+
+(def sum (map (comp sum-calories get-calories) elves))
+(def top3 (take 3 (sort (comp - compare) (doall sum))))
+(println (reduce + top3))
+
+;; (println (reduce max sum))
---
+++ 6/core.clj
@@ -0,0 +1,22 @@
+(def detect-len 14)
+(defn all-different [chrs] (
+                            if (= (count chrs) 2)
+                            (not= (first chrs) (last chrs))
+                            (and 
+                             (all-different (drop 1 chrs))
+                             (every? #(not= (first chrs) %) (drop 1 chrs)))))
+
+;; (def input "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+(def input (slurp "input.txt"))
+(def s (seq input))
+
+(def result (reduce (fn [vec e]
+                      (if (all-different (first vec))
+                        (reduced vec)
+                        [(concat (drop 1 (first vec)) [e]) (+ (last vec) 1)]))
+                    (vector (take detect-len s) detect-len)
+                    (drop detect-len s)))
+
+(print result)
+;; reduce f a = f (a[1]) (reduce f (a[1:]))
+;; where f: <a b> => a
